244105ae4206a1adaa6157f68215be1e30e70e75
simkyuwon <tlarbdnjs999@gmail.com>
2020-01-13 14:47:06 +0900

    Add tables

--- myHome/models.py
+++ myHome/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    serviceNumber = models.CharField(max_length=15, primary_key=True)
+    name = models.CharField(max_length=20)
+
+class FireExtinguisher(models.Model):
+    name = models.CharField(max_length=20)
+    inspectionDate = models.DateField()
+    inspector = models.ForeignKey(User, on_delete = models.SET_NULL, null=True)
